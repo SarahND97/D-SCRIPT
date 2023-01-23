c1d5c44144c88327396e9df5ac762f04be09bcc1
sarah <sarahnd97@gmail.com>
2023-01-23 10:34:05 +0100

    add code for shuffling fasta file

--- shuffle_code.py
+++ shuffle_code.py
@@ -5,6 +5,7 @@ Code for shuffling the user inputed dataset
 import argparse
 from sklearn.utils import shuffle
 import pandas as pd
+import Bio
 
 def add_args(parser):
     """
@@ -24,20 +25,33 @@ def main(args):
     Shuffle data stated by user 
     """
     data = args.data
-    seed = args.seed
+    seed = int(args.seed)
     output_path = args.o
     split_data = data.split("/")
 
     # Check that the file is a tsv-file
-    if not data.endswith('.tsv'):
-        return "Please use a tsv-file"
-    df = pd.read_table(data,header=None)
-    df = shuffle(df, random_state=seed)
-    if output_path is None:
-        output_path = "shuffled_"+split_data[-1]
-    df.to_csv(path_or_buf=output_path, sep='\t', header=False, index=False)
+    if not data.endswith('.tsv') or not data.endswith('.fasta') :
+        return "Please input  a tsv- or fasta-file"
+   
+    if data.endswith('.tsv')
+        df = pd.read_table(data,header=None)
+        df_copy = df.copy()
+        df = shuffle(df_copy, random_state=seed)
+        if output_path is None:
+            output_path = "shuffled_"+split_data[-1]
+        df.to_csv(path_or_buf=output_path, sep='\t', header=False, index=False)
+    else: 
+        with open('sequences.fasta') as fasta_file: 
+            df = []
+            for seq_record in SeqIO.parse(fasta_file, 'fasta'):  
+                df.append(seq_record)
+            df = shuffle(df, random_state=seed)
+            df.to_csv(path_or_buf=output_path, sep=',', header=False, index=False)
+
+                    
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     add_args(parser)
     main(parser.parse_args())
+
