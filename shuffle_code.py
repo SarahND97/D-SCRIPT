2212049477011cfed65e6f2e0f70278d9dfce471
sarah <sarahnd97@gmail.com>
2023-01-23 10:51:59 +0100

    add fixes for shuffle_code

--- shuffle_code.py
+++ shuffle_code.py
@@ -5,7 +5,7 @@ Code for shuffling the user inputed dataset
 import argparse
 from sklearn.utils import shuffle
 import pandas as pd
-import Bio
+from Bio import SeqIO
 
 def add_args(parser):
     """
@@ -25,14 +25,14 @@ def main(args):
     Shuffle data stated by user 
     """
     data = args.data
-    seed = int(args.seed)
+    seed = (int(args.seed) if args.seed is not None else None) # a if condition else b
     output_path = args.o
     split_data = data.split("/")
 
     # Check that the file is a tsv-file
-    if not data.endswith('.tsv') or not data.endswith('.fasta'):
+    if not data.endswith('.tsv') and not data.endswith('.fasta'):
         return "Please input  a tsv- or fasta-file"
-   
+    
     if data.endswith('.tsv'):
         df = pd.read_table(data,header=None)
         df_copy = df.copy()
@@ -41,12 +41,14 @@ def main(args):
             output_path = "shuffled_"+split_data[-1]
         df.to_csv(path_or_buf=output_path, sep='\t', header=False, index=False)
     else: 
-        with open('sequences.fasta') as fasta_file: 
+        with open(data) as fasta_file: 
             df = []
             for seq_record in SeqIO.parse(fasta_file, 'fasta'):  
                 df.append(seq_record)
             df = shuffle(df, random_state=seed)
-            df.to_csv(path_or_buf=output_path, sep=',', header=False, index=False)
+            if output_path is None:
+                output_path = "shuffled_"+split_data[-1]
+            SeqIO.write(df, output_path, "fasta")
 
                     
     
