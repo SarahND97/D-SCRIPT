fabf500697f162ff4be1e7bc77a590a6d7e6d4c2
sarah <sarahnd97@gmail.com>
2023-03-10 11:41:08 +0100

    add code for reformatting .fasta-files for negative samples

--- handle_files_rust/src/bin/change_fasta_files.rs
+++ handle_files_rust/src/bin/change_fasta_files.rs
@@ -21,7 +21,7 @@ fn main(){
     
     // Input is a directory and we want to read every -fasta-file in the directory
     for entry in fs::read_dir(dir_).unwrap() {
-        let mut outfile: String = args[2].to_owned();
+        // let mut outfile: String = args[2].to_owned();
         if let Ok(lines) = read_lines(entry.unwrap().path()) {
             // Consumes the iterator, returns an (Optional) String
             let mut i = 0;
@@ -31,13 +31,11 @@ fn main(){
                 if let Ok(ip) = line {
                     // println!("{}", ip);
                     if ip.starts_with('>') && i==0 {
-                        let ip_title = rem_first_and_last(&ip);
+                        let ip_title = rem_first(&ip);
                         title.push_str(">");
                         title.push_str(&ip_title);
                         title.push_str("_");
                         // input.push_str("\n");   
-                        outfile.push_str(&ip_title);
-                        outfile.push_str(".fasta");
                         // writeln!(to_write_file, "{}", input).unwrap();
                     }
                     else {
@@ -46,9 +44,9 @@ fn main(){
                             input.push_str(":");
                         }
                         if i==2 {
-                            let ip_title = rem_first_and_last(&ip)
-                            title.push_str(ip)
-                            title.push_str("\n"); 
+                            let ip_title = rem_first_and_last(&ip);
+                            title.push_str(&ip_title);
+                            title.push_str("\n");
                         }
                         if i==3 {
                             input.push_str(&ip);
@@ -62,15 +60,19 @@ fn main(){
                 }
                 i = i + 1;
             }
+            let outfile = rem_first_and_last(&title);
+            //outfile.to_string().push_str(".fasta");
+            //println!("{}",ourdir_.to_owned()+&outfile+"B"+".fasta");
             let mut to_write_file = BufWriter::new(
                 OpenOptions::new()
                 .append(true)
                 .create(true) // Optionally create
-                .open(ourdir_.to_owned()+&outfile)
+                .open(ourdir_.to_owned()+&outfile+"B"+".fasta")
                 .expect("Unable to open file"),
             );
-            writeln!(to_write_file, "{}", title.push_str(input)).unwrap();
-            // writeln!(to_write_file, "{}", input.to_string() + "\t" + "1.0").unwrap();
+            title.push_str(&input);
+            writeln!(to_write_file, "{}", title).unwrap();
+            //writeln!(to_write_file, "{}", input.to_string() + "\t" + "1.0").unwrap();
         }
     }
 }
@@ -84,6 +86,14 @@ where P: AsRef<Path>, {
 }
 
 fn rem_first_and_last(value: &str) -> &str {
+    let mut chars = value.chars();
+    chars.next();
+    chars.next_back();
+    // chars.next_back();
+    chars.as_str()
+}
+
+fn rem_first(value: &str) -> &str {
     let mut chars = value.chars();
     chars.next();
     // chars.next_back();
