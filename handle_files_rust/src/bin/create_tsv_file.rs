dd5b52e648dd86b42e23a14083e1fabe4f5df220
sarah <sarahnd97@gmail.com>
2023-03-28 17:27:50 +0200

    save update to create_tsv_file

--- handle_files_rust/src/bin/create_tsv_file.rs
+++ handle_files_rust/src/bin/create_tsv_file.rs
@@ -10,6 +10,7 @@ fn main(){
     let args: Vec<String> = env::args().collect();
     let dir_: &String = &args[1];
     let outfile: &String = &args[2];
+    let interact: &String = &args[3];
     
     // this allows to open a file in file append mode to write (or create it)
     let mut to_write_file = BufWriter::new(
@@ -20,24 +21,21 @@ fn main(){
         .expect("Unable to open file"),
     );
     
+    // Input is a directory and we want to read every -fasta-file in the directory
     for entry in fs::read_dir(dir_).unwrap() {
-        // let contents = fs::read_to_string(entry.unwrap().path())
-        // .expect("Should have been able to read the file");
+
         if let Ok(lines) = read_lines(entry.unwrap().path()) {
             // Consumes the iterator, returns an (Optional) String
             let mut i = 0;
             let mut input: String = "".to_owned();
             for line in lines {
                 if let Ok(ip) = line {
+                    // println!("{}", ip);
                     if ip.starts_with('>') && i==0 {
-                        //writeln!(to_write_file, "{}", ip.trim()).unwrap();
-                        //writeln!(to_write_file, "{}", "\t").unwrap();
                         let ip = rem_first_and_last(&ip);
                         input.push_str(ip);
                         input.push_str("\t");
-                        
                         // writeln!(to_write_file, "{}", ip.trim_start().to_string() + "\t").unwrap();
-                        //println!("{}", ip);
                     }
                     if ip.starts_with('>') && i==2 {
                         let ip = rem_first_and_last(&ip);
@@ -46,7 +44,7 @@ fn main(){
                 }
                 i = i + 1;
             }
-            writeln!(to_write_file, "{}", input.to_string() + "\t" + "1.0").unwrap();
+            writeln!(to_write_file, "{}", input.to_string() + "\t" + interact).unwrap();
         }
     }
 }
